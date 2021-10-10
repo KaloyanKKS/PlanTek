6c792a1bebe51d20ee4f8a4ead11811d493a9060
KaloyanKKS <k.yunchov9@gmail.com>
2021-10-10 13:57:26 +0100

    Initial commit

---
+++ src/main/java/llamarama/team/plantek/Plantek.java
@@ -0,0 +1,10 @@
+package llamarama.team.plantek;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Plantek implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/llamarama/team/plantek/client/PlantekClient.java
@@ -0,0 +1,13 @@
+package llamarama.team.plantek.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class PlantekClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
