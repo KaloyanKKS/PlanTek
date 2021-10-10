a610d3c740dd325c010cdc2b375e17fab1c7aec7
0xJoeMama <peterplastiras2006@gmail.com>
2021-10-10 17:24:19 +0300

    Further mod setup.

---
+++ src/main/java/io/github/llamarama/team/plantek/PlanTek.java
@@ -0,0 +1,22 @@
+package io.github.llamarama.team.plantek;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class PlanTek implements ModInitializer {
+
+    public static final String MODID = "plantek";
+    private static final Logger LOGGER = LogManager.getLogger("PlanTek");
+
+    public static Logger getLogger() {
+        return LOGGER;
+    }
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("And you thought plants couldn't get more interesting...");
+        LOGGER.info("Oh wait... you didn't think that?!");
+    }
+
+}
--- src/main/java/io/github/llamarama/team/plantek/client/PlanTekClient.java
+++ src/main/java/io/github/llamarama/team/plantek/client/PlanTekClient.java
@@ -1,13 +1,15 @@
-package llamarama.team.plantek.client;
+package io.github.llamarama.team.plantek.client;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
 @Environment(EnvType.CLIENT)
-public class PlantekClient implements ClientModInitializer {
+public class PlanTekClient implements ClientModInitializer {
+
     @Override
     public void onInitializeClient() {
 
     }
+
 }
