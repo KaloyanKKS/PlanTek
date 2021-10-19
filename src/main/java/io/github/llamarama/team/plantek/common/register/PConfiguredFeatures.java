c94e6949a8e451f470fe1ee6807cfda122cf3ff6
KaloyanKKS <k.yunchov9@gmail.com>
2021-10-19 23:57:01 +0100

    Textures, Roots are more destructive + other changes

--- src/main/java/io/github/llamarama/team/plantek/common/register/PConfiguredFeatures.java
+++ src/main/java/io/github/llamarama/team/plantek/common/register/PConfiguredFeatures.java
@@ -19,13 +19,13 @@ public class PConfiguredFeatures {
     public static final ConfiguredFeature<?, ?> ROOTS = register("roots", Feature.SIMPLE_BLOCK.configure(
             new SimpleBlockFeatureConfig(
                     new WeightedBlockStateProvider(pool()
-                            .add(Blocks.ROOTED_DIRT.getDefaultState(), 10).add(PBlocks.ROOT_BLOCK.getDefaultState().with(RootBlock.LIT, false), 7)
-                            .add(Blocks.MOSS_CARPET.getDefaultState(), 25).add(Blocks.DIRT.getDefaultState(), 50)
+                            .add(Blocks.ROOTED_DIRT.getDefaultState(), 10).add(PBlocks.ROOT_BLOCK.getDefaultState().with(RootBlock.LIT, false), 3)
+                            .add(Blocks.MOSS_CARPET.getDefaultState(), 10).add(Blocks.DIRT.getDefaultState(), 50)
                             .add(Blocks.ROOTED_DIRT.getDefaultState(), 10)))));
 
     public static final ConfiguredFeature<?, ?> ROOT_SPREAD = register("root_spread", Feature.VEGETATION_PATCH.configure(
             new VegetationPatchFeatureConfig(BlockTags.MOSS_REPLACEABLE.getId(), new SimpleBlockStateProvider(PBlocks.ROOT_BLOCK.getDefaultState().with(RootBlock.LIT, false)), () -> ROOTS,
-                    VerticalSurfaceType.FLOOR, ConstantIntProvider.create(1), 0.0F, 5, 0.6F, UniformIntProvider.create(1, 2), 0.75F)));
+                    VerticalSurfaceType.FLOOR, ConstantIntProvider.create(4), 1.0F, 5, 0.6F, UniformIntProvider.create(1, 2), 1.0F)));
 
     private static <FC extends FeatureConfig> ConfiguredFeature<FC, ?> register(String id, ConfiguredFeature<FC, ?> configuredFeature) {
         return Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, id, configuredFeature);
